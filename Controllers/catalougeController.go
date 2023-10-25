888d6b7d2aca177e7c8568e81da73b588e0cf530
saarthak <lci2021035@iiitl.ac.in>
2023-10-25 23:49:07 +0530

    basic catalouge routes complete

--- Controllers/catalougeController.go
+++ Controllers/catalougeController.go
@@ -1,38 +1,55 @@
 package Controllers
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
+	"google.golang.org/api/iterator"
 	"hs/headstart/Database"
 	"hs/headstart/Utils"
-	"log"
+	"mime/multipart"
 	"net/http"
 )
 
 func GetAllCatalouges(ctx *gin.Context) {
-	// TODO: fetch info from firestore
 	data := make([]map[string]any, 0)
-	data = append(data, gin.H{
-		"downloadURL": "abc.com",
-		"imageURL":    "https://nextjstemplates.com/_next/image?url=https%3A%2F%2Fapi.nextjstemplates.com%2Fimage%2FStartup-Pro---Next.js-Starter-Template-for-SaaS-Startups-282e26f7-f543-4ae4-a777-ac306c08cce8.png&w=1920&q=100",
-		"name":        "Lorem ipsum dolor sit amet",
-		"description": "Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.",
-	})
-	data = append(data, gin.H{
-		"downloadURL": "abc.com",
-		"imageURL":    "https://nextjstemplates.com/_next/image?url=https%3A%2F%2Fapi.nextjstemplates.com%2Fimage%2FTaxonomy---Next.js-13-Boilerplate-and-Template-f40f4bbe-ef9b-4335-b019-69d014801670.png&w=1920&q=10https://firebasestorage.googleapis.com/v0/b/clickcraft-ac100.appspot.com/o/images%2FStartup---Free-Next.webp?alt=media&token=6776a4d4-80a9-4519-92ca-d6e27e77610b0",
-		"name":        "Lorem ipsum dolor sit amet",
-		"description": "Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.",
-	})
+	iter := Database.Fs.Collection("Prebuilt").Documents(ctx)
+	var err error
+	for {
+		doc, itrErr := iter.Next()
+		if itrErr == iterator.Done {
+			break
+		}
+		if itrErr != nil {
+			err = itrErr
+		}
+		data = append(data, doc.Data())
+	}
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
 	ctx.JSON(http.StatusOK, data)
 }
 
-// type CatalougeForm struct {
-// 	name        string                `form:"name" binding:"required"`
-// 	description string                `form:"description" binding:"required"`
-// 	image       *multipart.FileHeader `form:"image" binding:"required"`
-// 	zip         *multipart.FileHeader `form:"zip" binding:"required"`
-// }
+type CatalougeObject struct {
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	ImageURL    string `json:"imageURL"`
+	ZipLocation string `json:"zipLocation"`
+}
+
+func handleUpload(fileHeader *multipart.FileHeader, uploadpath string) error {
+	fileBuffer, err := Utils.MultipartFileBuffer(fileHeader)
+	err = Database.UploadToCloudStorage(uploadpath+fileHeader.Filename, fileBuffer)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// TODO: Implement cleanUp
+func cleanUp() {
+
+}
 
 func PostACatalouge(ctx *gin.Context) {
 	// TODO: do sanitization
@@ -41,33 +58,18 @@ func PostACatalouge(ctx *gin.Context) {
 	imageHeader, _ := ctx.FormFile("image")
 	zipHeader, _ := ctx.FormFile("zip")
 
-	imgBuffer, err := Utils.MultipartFileBuffer(imageHeader)
-	if err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-
-	zipBuffer, err := Utils.MultipartFileBuffer(zipHeader)
-	if err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-	}
-
-	if err :=
-		Database.UploadToCloudStorage("images/"+imageHeader.Filename, imgBuffer); err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-
-	if err :=
-		Database.UploadToCloudStorage("Prebuilt/"+zipHeader.Filename, zipBuffer); err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
+	// TODO: add go routine for each of them
+	err := handleUpload(imageHeader, "image/")
+	err = handleUpload(zipHeader, "Prebuilt/")
 	imagePublicUrl := Database.PublicURL("images/" + imageHeader.Filename)
 
+	firestoreObject := CatalougeObject{name, description, imagePublicUrl, "Prebuilt/" + zipHeader.Filename}
+	_, _, err = Database.Fs.Collection("Prebuilt").Add(ctx, firestoreObject)
 
-	fmt.Printf("%s uploaded to bucket.\n", imageHeader.Filename)
-	log.Printf("%s %s %s %s", name, description, imageHeader.Filename, zipHeader.Filename)
-	log.Printf("%s\n", imagePublicUrl)
-
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		cleanUp()
+		return
+	}
+	ctx.JSON(http.StatusCreated, firestoreObject)
 }
